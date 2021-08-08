d0cc97cddccffdf4cdbb428e06ef231682df4d42
samdtech <opeyemisamuel222@gmail.com>
2021-08-08 21:57:55 +0100

    bug around reply mail fixed

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {Email} from "../email";
+import { Component, Input, OnInit } from '@angular/core';
+import { Email } from '../email';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
-  styleUrls: ['./email-reply.component.css']
+  styleUrls: ['./email-reply.component.css'],
 })
-export class EmailReplyComponent implements OnInit {
-public showModal: boolean = false;
-public email: Email | any
-  constructor() { }
+export class EmailReplyComponent  {
+  @Input() email: Email | any;
+  public showModal: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private emailService: EmailService) {}
+
+  ngOnChanges(): void {
+    const text = this.email.text.replace(/\n/gi, '\n> ')
+
+    this.email = {
+      ...this.email,
+      to: this.email.from,
+      from: this.email.to,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n--------- ${this.email.from} wrote:\n> ${text}`,
+    };
   }
 
-  onSubmit(email: Email){
-
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(()=>{
+      this.showModal= false
+    })
   }
-
 }
